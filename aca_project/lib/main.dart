c8fa0c216f08a2d058484289187848bea9b7ee4b
Basavakiran Shivanand Deshmukh <basavakirandeshmukh@gmail.com>
2025-06-19 15:15:43 +0530

    Created Saperate login dart file

--- aca_project/lib/main.dart
+++ aca_project/lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:aca_project/firebase_options.dart';
 import 'package:aca_project/login_material_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(MyApp());
 }
 
